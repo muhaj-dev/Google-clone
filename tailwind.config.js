8adddb9d70dc033d25110259cdc54c15185f09a9
muhammod1 <muhammodholawale@gmail.com>
2022-05-05 23:41:13 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   darkMode: 'class',
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
