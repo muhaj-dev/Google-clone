51d547a7560497583aa1c45e40556aa2bc3b7a8b
muhammod1 <muhammodholawale@gmail.com>
2022-05-06 07:20:33 +0100

    component setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+      <h1>2022 Google, Inc.</h1>
+      <h1>Built with React, By <span className='text-2xl font-bold'>MUHAJ</span></h1>
+    </div>
   )
 }
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import {Results} from './Results';
 
-export const Routes = () => {
+export const Routers = () => {
   return (
-    <div>Routes</div>
+    <div className='p-4'>
+      <Routes>
+        <Route path="/" element={<Navigate to="/search" />} />
+        <Route path='/search' element={<Results />} />
+        <Route path='/images' element={<Results />} />
+        <Route path='/news' element={<Results />} />
+        <Route path='/videos' element={<Results />} />
+      </Routes>
+    </div>
   )
 }
-//ugiuoyhoi
