c2861bed8e7d387b1b9fb38cd9335971b0f0f8c6
muhammod1 <muhammodholawale@gmail.com>
2022-05-08 11:03:32 +0100

    api implementation

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -26,7 +26,7 @@ export const Results = () => {
     case '/search':
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {results?.results?.map(({link, title}, index) => (
+          {results?.map(({link, title}, index) => (
             <div className='md:w-2/5 w-full' key={index}>
               <a href={link} target="_blank" rel='noreferrer'>
                 <p className='text-sm'>
@@ -43,7 +43,7 @@ export const Results = () => {
     case '/image':
       return (
         <div className="flex flex-wrap justify-center items-center">
-          {results?.image_results?.map(({ image, link: { href, title } }, index) => (
+          {results?.map(({ image, link: { href, title } }, index) => (
             <a href={href} target="_blank" key={index} rel="noreferrer" className="sm:p-3 p-5">
               <img src={image?.src} alt={title} loading="lazy" />
               <p className="sm:w-36 w-36 break-words text-sm mt-2">
@@ -54,9 +54,34 @@ export const Results = () => {
         </div>
       );
     case '/news':
-      return 'Search';
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 item-center'>
+          {results?.map(({links, id, source, title}) => (
+            <div className='md:w-2/5 w-full' key={id}>
+              <a href={links?.[0].href} target="_blank" rel='noreferrer' className='hover:underline'>
+                <p className='text-lg dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+                <div className='flex gap-4'>
+                  <a href={source?.href} target="_blank" rel='noreferrer'>
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      )
     case '/videos':
-      return 'Search';
+      return (
+        <div className='flex flex-wrep'>
+          {results.map((video, index) => (
+            <div key={index} className='p-2'>
+              <ReactPlayer url={video.additional_links?.[0].href} control width='355px' height='200px' />
+            </div>
+          ))}
+        </div>
+      )
   
     default:
       return 'Error';
--- src/components/contexts/ResultContextProvider.js
+++ src/components/contexts/ResultContextProvider.js
@@ -25,7 +25,14 @@ export const ResultContextProvider =  ({ children }) => {
         const data = await response.json();
 
         console.log(data)
-        setResults(data);
+        if(type.includes('/news')) {
+            setResults(data.entries)
+        } else if(type.includes('/image')) {
+            setResults(data.images_results)
+        } else {
+            setResults(data.results)
+        }
+
         setIsLoading(false);
     }
 
