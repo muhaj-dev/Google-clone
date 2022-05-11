c803286e572b1cf24dd485cf937bbbcd7b02aa0a
muhammod1 <muhammodholawale@gmail.com>
2022-05-11 18:17:22 +0100

    nav button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 import { Links } from "./Links"
 import { NavLink } from 'react-router-dom';
 
-const links = [
-  { url: '/search', text: 'All' },
-  { url: '/image', text: 'All' },
-  { url: '/news', text: 'All' },
-  { url: '/videos', text: 'All' },
-];
 export const Search = () => {
   return (
     <div>
