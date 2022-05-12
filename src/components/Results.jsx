ad16954459652e64a0c472f664105aaaa837c95a
muhammod1 <muhammodholawale@gmail.com>
2022-05-12 20:04:05 +0100

    code update

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -27,7 +27,7 @@ export const Results = () => {
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
           {results?.map(({link, title}, index) => (
-            <div className='md:w-2/5 w-full' key={index}>
+            <div className='md:w-3/4 w-full' key={index}>
               <a href={link} target="_blank" rel='noreferrer'>
                 <p className='text-sm'>
                   {link.length > 30 ? link.substring(0, 30) : link}
